c28942f71cf6a25a66596caa0cfa98a06b7b51b3
ves <58036050@qq.com>
2014-10-06 02:20:27 +0800

    zz

--- lib/ftp.class.php
+++ lib/ftp.class.php
@@ -11,6 +11,7 @@ class Ftp{
 			# encode an random password
 			$pwd = $this->rand_password(6);
 
+			
 
 
 		}
@@ -23,6 +24,63 @@ class Ftp{
 		$pwd = substr(md5(time()),0,6);
 		return $pwd;
 	}
+
+	/*
+	 writing log..
+	 return 1  SUCC
+	 return -1  LOSE PARAM
+	 return -2  FILE_NOT EXITST
+	*/
+	private function write_log($msg='',$act='',$path_file=''){
+		if($msg!='' && $act!='' && $path_file!=''){
+			if(file_exists($path_file)){
+				file_put_contents($path,$msg.'\n',FILE_APPEND);
+				return 1;
+			}
+			else{
+				return -2;
+			}
+		}
+		else{
+			return -1;
+		}
+	}
+
+	/*
+	  Do a HTPASS COMMAND
+
+	  @username	  USERNAME
+	  @pwd		  PWD
+	  @file		  SAVE FILE
+	  return 1  SUCC
+	  return -1  LOSE PARAM
+	  return -2  FILE_NOT EXITST
+	  return -3  EXCUTE WRONG
+	*/
+	private function HTPASS($username,$pwd,$file){
+		if($username!='' && $pwd!='' && $file!=''){
+			if(file_exists($file)){
+				$is='';
+				system('htpasswd -bd '.$file.' '.$username.' '.$pwd.'',$is);
+				if($is==0){
+					return 1;
+				}
+				else{
+					return -3;
+				}
+			}
+			else{
+				return -2;
+			}
+		}
+		else{
+			return -1;
+		}
+	}
+
+	#private function 
+	//private function 
+
 }
 
 $a=new Ftp();
